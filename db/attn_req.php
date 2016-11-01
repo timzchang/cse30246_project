61f5dce65ebacd7a8d5b07e69dfa3ba55fa18dea
Christopher Andrew Syers <csyers@dsg1.virtual.crc.nd.edu>
2016-11-01 16:09:50 -0400

    attn_req.php

--- db/attn_req.php
+++ db/attn_req.php
@@ -4,9 +4,16 @@ $eventid = $_POST["eventid"];
 $absence_type = $_POST["absence_type"];
 $excused = $_POST["excused"];
 
-echo $netid;
-echo $eventid;
-echo $absence_type;
-echo $excused;
- 
+$link = mysqli_connect('localhost','csyers','trombone') or die('Could not connect: ' . mysql_error());
+
+mysqli_select_db($link,'databse') or die('Could not select databse');
+
+$eventid = intval($eventid);
+
+$sql = "INSERT INTO attendance_issues VALUES ($netid,$eventid,$absence_type,$excused);";
+
+$result = mysqli_query($link,$sql) or die('Query failed: ' . mysql_error());
+
+echo $result;
+
 ?>
