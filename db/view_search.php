78b52277d184416f2ecf4cb1f7adb26c3cd3f8fb
timzchang <zhaotingchang@gmail.com>
2016-11-29 13:20:25 -0500

    table return from server[4]: add date/type fields to

--- db/view_search.php
+++ db/view_search.php
@@ -1,15 +1,16 @@
 <?php
 $netid = $_GET["netid"];
 
-//if (!$netid) {
-//    return;
-//}
+if (!$netid) {
+    echo "none";
+    return;
+}
 
 $link = mysqli_connect('localhost','csyers','trombone') or die('Could not connect: ' . mysql_error());
 
 mysqli_select_db($link,'databse') or die('Could not select databse');
 
-$sql = "SELECT * FROM attendance_issues WHERE attendance_issues.netid LIKE \"%$netid%\";";
+$sql = "SELECT * FROM attendance_issues a, events e WHERE a.netid=e.netid and attendance_issues.netid LIKE \"%$netid%\";";
 
 $result = mysqli_query($link,$sql) or die('Query failed: ' . mysql_error());
 $response = array();
@@ -23,23 +24,5 @@ if ($result && mysqli_num_rows($result) > 0) {
 
 header('Content-Type: application/json');
 echo json_encode($response);
-
-// echo "<table class=\"table table-bordered\">\n";
-// echo"    <thead>
-//       <tr>
-//         <th>netid</th>
-//         <th>Event</th>
-//         <th>Late/Absent</th>
-// 	<th>Excused?</th>
-//       </tr>
-//     </thead>";
-// while ($tuple = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-//     echo "\t<tr>\n";
-//     foreach ($tuple as $col_value) {
-//         echo "\t\t<td>$col_value</td>\n";
-//     }
-//     echo "\t</tr>\n";
-// }
-// echo "</table>\n";
 
 ?>
--- scripts/view.js
+++ scripts/view.js
@@ -18,54 +18,37 @@ function submitForm () {
             netid: netid
         }
     }).done(function(attn, status){
-	if (!Array.isArray(attn)) {
-		console.log("not array");
-	}
-	var $table = $("table tbody").html('');
-    //$table += "<table class=\"table table-bordered\">\n";
-    //$table += "    <thead> <tr> <th>netid</th> <th>Event</th> <th>Late/Absent</th> <th>Excused?</th> </tr> </thead>";
+    	if (!Array.isArray(attn)) {
+            if (attn == "none") {
+                console.log("invalid netid");
+                $('.alert-danger .msg').html('Failed to connect to database!');
+                $('.alert-danger').slideDown();
+                return;
+            };
+    		console.log("not array");
+            return;
+    	}
 
-    for (var i = 0; i < attn.length; i++) {
-	var excused = (attn[i].excused == 'Y') ? "Yes" : "No";
-	var absence_type = (attn[i].absence_type == 'A') ? "Absent" : "Late";
-        $row = $('<tr id="row-' + i + '">'+
-            '<td>' + attn[i].netid + '</td>' +
-            '<td>' + attn[i].eventid + '</td>' +
-            '<td>' + absence_type + '</td>' +
-            '<td>' + excused + '</td>'+
-            '<td><a class="edit-issue" href="#"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' +
-            '<a class="remove-issue" href="#"><span class="glyphicon glyphicon-trash"></span></td><tr>'
-            );
+    	var $table = $("table tbody").html('');
 
-        $row.find('.edit-issue').data(attn[i]);
-        $row.find('.remove-issue').data(attn[i]);
-	//console.log($row);
+        for (var i = 0; i < attn.length; i++) {
+    	    var excused = (attn[i].excused == 'Y') ? "Yes" : "No";
+    	    var absence_type = (attn[i].absence_type == 'A') ? "Absent" : "Late";
+            var date = attn[i].start.split()[0];
+            $row = $('<tr id="row-' + i + '">'+
+                '<td>' + attn[i].netid + '</td>' +
+                '<td>' + date + '</td>' +
+                '<td>' + attn[i].type + '</td>' +
+                '<td>' + absence_type + '</td>' +
+                '<td>' + excused + '</td>'+
+                '<td><a class="edit-issue" href="#"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' +
+                '<a class="remove-issue" href="#"><span class="glyphicon glyphicon-trash"></span></td><tr>'
+                );
 
-        //$("#resp_table").html($row);
-	$table.append($row);
-    };
-    //$("#resp_table").html($table);
-
-	
-        
-        // echo "<table class=\"table table-bordered\">\n";
-        // echo"    <thead>
-        //       <tr>
-        //         <th>netid</th>
-        //         <th>Event</th>
-        //         <th>Late/Absent</th>
-        //  <th>Excused?</th>
-        //       </tr>
-        //     </thead>";
-        // while ($tuple = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-        //     echo "\t<tr>\n";
-        //     foreach ($tuple as $col_value) {
-        //         echo "\t\t<td>$col_value</td>\n";
-        //     }
-        //     echo "\t</tr>\n";
-        // }
-        // echo "</table>\n";
-        // $("#resp_table").html(data);
+            $row.find('.edit-issue').data(attn[i]);
+            $row.find('.remove-issue').data(attn[i]);
+        	$table.append($row);
+        };
     }).fail(function (err) {
         console.log(err);
         $('.alert-danger .msg').html('Failed to connect to database!');
