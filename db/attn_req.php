fceed7393e377d7f8184e6880ccae04c7b578c88
Christopher Andrew Syers <csyers@dsg1.virtual.crc.nd.edu>
2016-12-03 18:26:28 -0500

    commented some files

--- db/attn_req.php
+++ db/attn_req.php
@@ -1,23 +1,43 @@
 <?php
+
+# get information from POST command
 $netid = $_POST["netid"];
-$eventid = $_POST["eventid"];
+$date = $_POST["date"];
 $absence_type = $_POST["absence_type"];
 $excused = $_POST["excused"];
 
+# link to dsg
 $link = mysqli_connect('localhost','csyers','trombone') or die('Could not connect: ' . mysql_error());
 
+# link to databse
 mysqli_select_db($link,'databse') or die('Could not select databse');
 
-$eventid = intval($eventid);
+# get the event idea from the events table
+$sql = "SELECT eventid FROM events WHERE date = '$date'";
 
-$sql = "INSERT INTO attendance_issues VALUES (\"$netid\",$eventid,\"$absence_type\",\"$excused\");";
+# run the SQL command
+$result = mysqli_query($link,$sql) or die('Query failed" ' . mysql_error());
 
-$result = mysqli_query($link,$sql) or die('Query failed: ' . mysql_error());
+# if there was an event that mathed the information
+if($result->num_rows != 0){
+    # get the first row
+    $row = $result->fetch_row();
+    $eventid = $row[0];
 
-if($result) {
-	echo "Created new attendance issue.";
+    $sql = "INSERT INTO attendance_issues VALUES (\"$netid\",$eventid,\"$absence_type\",\"$excused\");";
+
+    $result = mysqli_query($link,$sql) or die('Query failed: ' . mysql_error());
+
+    if($result) {
+    	echo "Created new attendance issue.";
+    } else {
+        echo "Error: attendance issue not created.";
+    }
 } else {
-	echo "Error: attendance issue not created.";
+    # if there was no event on that date, return that information to the user
+    $date = date_create_from_format('Y-m-d',$date);
+    $date = $date->format('l, F d, Y');
+    echo "There is no $event_type event on $date.";
 }
 
 ?>
--- db/view_search.php
+++ db/view_search.php
@@ -1,32 +1,42 @@
 <?php
+# get information from GET command
 $netid = $_GET["netid"];
 
+# case: netid is empty
 if (!$netid) {
     return;
 }
 
+# link to dsg
 $link = mysqli_connect('localhost','csyers','trombone') or die('Could not connect: ' . mysql_error());
 
+# connect to databse
 mysqli_select_db($link,'databse') or die('Could not select databse');
 
+# check if there is a student in the DB with that student's nnetid
 $test = "SELECT * FROM students WHERE netid LIKE \"$netid\";";
 $test_result = mysqli_query($link,$test) or die('Test failed: ' . mysql_error());
 
+# return error if no students are in the list
 if (!$test_result || mysqli_num_rows($test_result) == 0) {
     echo "none";
     return;
 }
 
+# SQL query to get all issues for this student
 $sql = "SELECT * FROM attendance_issues a, events e WHERE a.eventid=e.eventid and a.netid LIKE \"%$netid%\";";
 
+# get the result of the query
 $result = mysqli_query($link,$sql) or die('Query failed: ' . mysql_error());
 $response = array();
 
+# if no issues exist:
 if ($result && mysqli_num_rows($result) == 0) {
     echo "empty";
     return;
 }
 
+# populate the response array with the rows from the query
 if ($result && mysqli_num_rows($result) > 0) {
     while ($row = $result->fetch_array()) {
         $response[] = $row;
