38e597bd065b4dd4c4bd278c9ffe4ae6eabfdaa5
Christopher Andrew Syers <csyers@dsg1.virtual.crc.nd.edu>
2016-12-03 18:32:41 -0500

    new php functionality to get event type of a given date

---
+++ db/get_event_type.php
@@ -0,0 +1,29 @@
+<?php
+
+# get the information from the POST command
+$date = $_POST["date"]
+
+# link to the database
+$link = mysqli_connect('localhost','csyers','trombone') or die('Could not connect: ' . mysql_error());
+
+# connect to the databse
+mysqli_select_db($link,'databse') or die('Could not select databse');
+
+# SQL query to get the event requested
+
+$sql = "SELECT type FROM events WHERE date = '$date'";
+
+# run the SQL query and store the result
+$result = mysqli_query($link,$sql) or die('Query failed" ' . mysql_error());
+
+if($result->num_rows != 0){
+    # get first row
+    $row = $result->fetch_row();
+    # get event type from the row
+    $type = $row[0];
+    echo $type;
+# if there is no event on the date, return error
+} else {
+    echo "No event";
+}
+?>
