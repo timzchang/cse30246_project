d89c47ef8b6b4fe0993494aa2a9f95d38753f529
Christopher Andrew Syers <csyers@dsg1.virtual.crc.nd.edu>
2016-12-03 17:51:59 -0500

    commented delete issue

--- db/delete_issue.php
+++ db/delete_issue.php
@@ -1,17 +1,24 @@
 <?php
+# get information from POST command
 $netid = $_POST["netid"];
 $eventid = $_POST["eventid"];
 
+# connect to dsg
 $link = mysqli_connect('localhost','csyers','trombone') or die('Could not connect: ' . mysql_error());
 
+# connect to databse
 mysqli_select_db($link,'databse') or die('Could not select databse');
 
+# convert eventid to an integer
 $eventid = intval($eventid);
 
+# SQL query to delete the attendance issue
 $sql = "DELETE FROM attendance_issues WHERE attendance_issues.netid = \"$netid\" AND attendance_issues.eventid = $eventid;";
 
+# run the query and get the result
 $result = mysqli_query($link,$sql) or die('Query failed: ' . mysql_error());
 
+# if success, tell the front end
 if($result){
 	echo "Deletion successful.";
 } else {
