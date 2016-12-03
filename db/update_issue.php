dc07e5ec2e9322d28da6f241588f8ad27998a644
Christopher Andrew Syers <csyers@dsg1.virtual.crc.nd.edu>
2016-12-03 12:21:20 -0500

    updated update script

--- db/update_issue.php
+++ db/update_issue.php
@@ -5,16 +5,19 @@ $event_type = $_POST["event_type"];
 $absence_type = $_POST["absence_type"];
 $excused = $_POST["excused"];
 
+echo $date;
+echo $event_type;
+echo $absence_type;
+echo $excused;
+
 $link = mysqli_connect('localhost','csyers','trombone') or die('Could not connect: ' . mysql_error());
 
 mysqli_select_db($link,'databse') or die('Could not select databse');
-
-$sql = "SELECT eventid FROM events WHERE date = $date";
+$sql = "SELECT eventid FROM events WHERE date = '$date' AND type = '$event_type'";
 $result = mysqli_query($link,$sql) or die('Query failed" ' . mysql_error());
-
-if($result->num_rows == 0){
-    $obj = $result->fetch_row();
-    $eventid = $obj->eventid;
+if($result->num_rows != 0){
+    $row = $result->fetch_row();
+    $eventid = $row[0];
     $sql = "UPDATE attendance_issues SET attendance_issues.absence_type = '$absence_type', attendance_issues.excused = '$excused' WHERE attendance_issues.netid = \"$netid\" AND attendance_issues.eventid = $eventid;";
 
     $result = mysqli_query($link,$sql) or die('Query failed: ' . mysql_error());
@@ -25,7 +28,9 @@ if($result->num_rows == 0){
 	    echo "Update failed.";
     }
 } else {
-    echo "There is not event on $date.";
+    $date = date_create_from_format('Y-m-d',$date);
+    $date = $date->format('l, F d, Y');
+    echo "There is no $event_type event on $date.";
 }
 
 ?>
