b770481c4315cdf701d29d872163ac5804571cea
Christopher Andrew Syers <csyers@dsg1.virtual.crc.nd.edu>
2016-12-02 21:41:29 -0500

    NOT WORKING YET

--- db/update_issue.php
+++ db/update_issue.php
@@ -1,6 +1,6 @@
 <?php
 $netid = $_POST["netid"];
-$eventid = $_POST["date"];
+$date = $_POST["date"];
 $event_type = $_POST["event_type"];
 $absence_type = $_POST["absence_type"];
 $excused = $_POST["excused"];
@@ -9,16 +9,12 @@ $link = mysqli_connect('localhost','csyers','trombone') or die('Could not connec
 
 mysqli_select_db($link,'databse') or die('Could not select databse');
 
-$eventid = intval($eventid);
-
 $sql = "SELECT eventid FROM events WHERE date = $date";
-
 $result = mysqli_query($link,$sql) or die('Query failed" ' . mysql_error());
 
 if($result->num_rows == 0){
-    $obj = $result->fetch_ojbect();
+    $obj = $result->fetch_row();
     $eventid = $obj->eventid;
-
     $sql = "UPDATE attendance_issues SET attendance_issues.absence_type = '$absence_type', attendance_issues.excused = '$excused' WHERE attendance_issues.netid = \"$netid\" AND attendance_issues.eventid = $eventid;";
 
     $result = mysqli_query($link,$sql) or die('Query failed: ' . mysql_error());
