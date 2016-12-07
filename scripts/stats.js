30e9adb4b0373ab0407be77da52ddd2a7fa8e3a3
Chris 'Scary' Scaramella <CScarame@nd.edu>
2016-12-06 23:54:22 -0500

    modified:   scripts/stats.js

--- scripts/stats.js
+++ scripts/stats.js
@@ -16,22 +16,6 @@ var Sections = {
  *
  * Why not try some of the options above?
  */
- Morris.Bar({
-  element: 'bar-example',
-  data: [
-    { y: '2006', a: 100, b: 90 },
-    { y: '2007', a: 75,  b: 65 },
-    { y: '2008', a: 50,  b: 40 },
-    { y: '2009', a: 75,  b: 65 },
-    { y: '2010', a: 50,  b: 40 },
-    { y: '2011', a: 75,  b: 65 },
-    { y: '2012', a: 100, b: 90 }
-  ],
-  xkey: 'y',
-  ykeys: ['a', 'b'],
-  labels: ['Series A', 'Series B'],
-  stacked: 'true'
-});
 
  Morris.Bar({
   element: 'bar-example2',
@@ -103,7 +87,7 @@ $(function () {
         sections: section_vals,
         excused: excused,
         unexcused: unexcused,
-        late: late, 
+        late: late,
         absent: absent
       }).done(function(absences, status){
         lineGraph(absences);
@@ -116,18 +100,23 @@ $(function () {
         end_date: $('#end-date').val()
       }).done(function(absences, status){
         //barGraph(absences);
-	$('#bar-example').html('');
-	$('#bar-example2').html('');
-	$bar_data = [];
-	$bar_ratio_data = [];
+	$('#bar-count').html('');
+	$('#bar-ratio').html('');
+	// Bar-count Variables
+	$bar_count_data = [];
 	$bar_ykeys = [];
 	$bar_labels = [];
 	$bar_colors = [];
+	// Bar-ratio Variables
+	$bar_ratio_data = [];
 	for( var i = 0; i < absences.length; i ++){
 		$temp_obj = {};
+		$ratio_obj = {};
 		$temp_obj.section = absences[i].section;
+		$ratio_obj.section = absences[i].section;
 		if(excused && late){
 			$temp_obj.excused_late = absences[i].excused_late;
+			$ratio_obj.excused_late = (absences[i].excused_late)/(absences[i].members);
 			if( i == 0){
 				$bar_ykeys.push('excused_late');
 				$bar_labels.push('Excused - Late');
@@ -135,37 +124,55 @@ $(function () {
 			}
 		} if(excused && absent){
 			$temp_obj.excused_absent = absences[i].excused_absent;
+			$ratio_obj.excused_absent = (absences[i].excused_absent)/(absences[i].members);
 			if( i == 0){
 			$bar_ykeys.push('excused_absent');
-			$bar_labels.push('Excused - Absent'); 
+			$bar_labels.push('Excused - Absent');
 			$bar_colors.push('darkblue')}
 		} if(unexcused && late){
 			$temp_obj.unexcused_late = absences[i].unexcused_late;
+			$ratio_obj.unexcused_late = (absences[i].unexcused_late)/(absences[i].members);
 			if( i == 0){
 			$bar_ykeys.push('unexcused_late');
-			$bar_labels.push('Unexcused - Late'); 
+			$bar_labels.push('Unexcused - Late');
 			$bar_colors.push('darkred'); }
 		}
 		if(unexcused && absent){
 			$temp_obj.unexcused_absent = absences[i].unexcused_absent;
+			$ratio_obj.unexcused_absent = (absences[i].unexcused_absent)/(absences[i].members);
 			if( i == 0){
 			$bar_ykeys.push('unexcused_absent');
-			$bar_labels.push('Unexcused - Absent'); 
+			$bar_labels.push('Unexcused - Absent');
 			$bar_colors.push('red');}
 		}
 		$bar_data.push($temp_obj);
 	}
 	 Morris.Bar({
-  		  element: 'bar-example',
-  		  data: $bar_data,
+  		  element: 'bar-count',
+  		  data: $bar_count_data,
 		  xkey: 'section',
 		  ykeys: $bar_ykeys,
 		  labels: $bar_labels,
 		  stacked: 'true',
 		  barColors: $bar_colors
 		});
-			
-		
+
+
+      }).fail(function (err) {
+        console.log("failed")
+        console.log(err);
+      });
+      Morris.Bar({
+  		  element: 'bar-ratio',
+  		  data: $bar_ratio_data,
+		  xkey: 'section',
+		  ykeys: $bar_ykeys,
+		  labels: $bar_labels,
+		  stacked: 'true',
+		  barColors: $bar_colors
+		});
+
+
       }).fail(function (err) {
         console.log("failed")
         console.log(err);
@@ -183,7 +190,7 @@ function lineGraph(attn, lin) {
   $('#attn-over-time').html('');
   sections = Object.keys(attn);
   console.log(attn);
-  
+
   data = [];
   for(p in attn[sections[0]]) {
     data.push({y: p});
