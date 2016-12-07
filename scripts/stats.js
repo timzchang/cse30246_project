068fe304f83e43e465e42e78b8732fa2c6aa908e
Christopher Ralph Scaramella <cscarame@dsg1.virtual.crc.nd.edu>
2016-12-07 00:50:24 -0500

    modified:   stats.js

--- scripts/stats.js
+++ scripts/stats.js
@@ -17,27 +17,10 @@ var Sections = {
  * Why not try some of the options above?
  */
 
- Morris.Bar({
-  element: 'bar-ratio',
-  data: [
-    { y: '2006', a: 100, b: 90 },
-    { y: '2007', a: 75,  b: 65 },
-    { y: '2008', a: 50,  b: 40 },
-    { y: '2009', a: 75,  b: 65 },
-    { y: '2010', a: 50,  b: 40 },
-    { y: '2011', a: 75,  b: 65 },
-    { y: '2012', a: 100, b: 90 }
-  ],
-  xkey: 'y',
-  ykeys: ['a', 'b'],
-  labels: ['Series A', 'Series B'],
-  stacked: 'true'
-});
-
 var lin = Morris.Line({
   element: 'attn-over-time',
   data: [
-  { y: '20016', a: 0 },
+  { y: '2016', a: 0 },
   ],
   xkey: 'y',
   ykeys: ['a'],
