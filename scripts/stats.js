8c273dd92d27de18d17037413fd4b097f66f85db
Chris 'Scary' Scaramella <CScarame@nd.edu>
2016-12-06 19:47:17 -0500

    Added bar graphs to stats.html and stats.js

--- scripts/stats.js
+++ scripts/stats.js
@@ -3,6 +3,40 @@
  *
  * Why not try some of the options above?
  */
+ Morris.Bar({
+  element: 'bar-example',
+  data: [
+    { y: '2006', a: 100, b: 90 },
+    { y: '2007', a: 75,  b: 65 },
+    { y: '2008', a: 50,  b: 40 },
+    { y: '2009', a: 75,  b: 65 },
+    { y: '2010', a: 50,  b: 40 },
+    { y: '2011', a: 75,  b: 65 },
+    { y: '2012', a: 100, b: 90 }
+  ],
+  xkey: 'y',
+  ykeys: ['a', 'b'],
+  labels: ['Series A', 'Series B'],
+  stacked: 'true'
+});
+
+ Morris.Bar({
+  element: 'bar-example2',
+  data: [
+    { y: '2006', a: 100, b: 90 },
+    { y: '2007', a: 75,  b: 65 },
+    { y: '2008', a: 50,  b: 40 },
+    { y: '2009', a: 75,  b: 65 },
+    { y: '2010', a: 50,  b: 40 },
+    { y: '2011', a: 75,  b: 65 },
+    { y: '2012', a: 100, b: 90 }
+  ],
+  xkey: 'y',
+  ykeys: ['a', 'b'],
+  labels: ['Series A', 'Series B'],
+  stacked: 'true'
+});
+
 Morris.Line({
   element: 'attn-over-time',
   data: [
