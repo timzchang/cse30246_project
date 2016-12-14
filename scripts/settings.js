f0fe02f3a9ed9b22725a31657bff9467a44ec85f
timzchang <zhaotingchang@gmail.com>
2016-12-14 15:21:04 -0500

    add event support/modals to settings

--- scripts/settings.js
+++ scripts/settings.js
@@ -5,6 +5,8 @@ $('#create-student').validator().on('submit', function(event) {
     } else {
         // everything looks good!
         event.preventDefault();
+        $('#student-error').slideUp();
+        $('#student-good').slideUp();
         createStudent();
     }
 });
@@ -17,9 +19,51 @@ $('#create-event').validator().on('submit', function(event) {
     } else {
         // everything looks good!
         event.preventDefault();
+        $('#error').slideUp();
+        $('#good').slideUp();
         createEvent();
     }
 });
+
+$("#student-closebtn").on('click', function (event) {
+    $('#student-error').slideUp();
+})
+$("#student-success-closebtn").on('click', function (event) {
+    $('#student-good').slideUp();
+})
+
+$("#closebtn").on('click', function (event) {
+    $('#error').slideUp();
+})
+$("#success-closebtn").on('click', function (event) {
+    $('#good').slideUp();
+})
+
+function createEvent() {
+    console.log($('#date').val());
+    $.post("../db/create_event.php",
+    {
+        event_type: $('#event-type').val(),
+        date: $('#date').val()
+    }).done(function (resp) {
+        if(Array.isArray(resp)) {
+            if(resp[0] == "0") {
+                $('#good .msg').html(resp[1]);
+                $('#good').slideDown();
+            } else {
+                $('#error .msg').html(resp[1]);
+                $('#error').slideDown();
+
+            } 
+            //alert(resp[1]);
+        } else {
+            $('#error .msg').html(resp);
+            $('#error').slideDown();
+        }
+    }).fail(function (err) {
+        console.log(err);
+    });
+}
 
 function createStudent() {
     $.post("../db/create_student.php",
@@ -33,9 +77,16 @@ function createStudent() {
         block: $('#block').val()
     }).done(function (resp) {
         if(Array.isArray(resp)) {
-            alert(resp[1]);
+            if(resp[0] == "0") {
+                $('#student-good .msg').html(resp[1]);
+                $('#student-good').slideDown();
+            } else {
+                $('#student-error .msg').html(resp[1]);
+                $('#student-error').slideDown();
+            } 
         } else {
-            alert(resp);
+            $('#student-error .msg').html(resp);
+            $('#student-error').slideDown();
         }
     }).fail(function (err) {
         console.log(err);
