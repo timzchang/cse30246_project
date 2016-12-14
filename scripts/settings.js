d9a8b9bc8123c29efc5c118a97fa5fe313813252
timzchang <zhaotingchang@gmail.com>
2016-12-14 14:51:56 -0500

    add create_student frontend

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,1 +1,43 @@
+$('#create-student').validator().on('submit', function(event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        console.log("something wrong");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        createStudent();
+    }
+});
 
+
+$('#create-event').validator().on('submit', function(event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        console.log("something wrong");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        createEvent();
+    }
+});
+
+function createStudent() {
+    $.post("../db/create_student.php",
+    {
+        lname: $('#last_name').val(),
+        fname: $('#first_name').val(),
+        section: $('section').val(),
+        netid: $('netid').val(),
+        yog: $('year').val(),
+        school: $('school').val(),
+        block: $('block').val()
+    }).done(function (resp) {
+        if(Array.isArray(resp)) {
+            alert(resp[1]);
+        } else {
+            alert(resp);
+        }
+    }).fail(function (err) {
+        console.log(err);
+    });
+}
